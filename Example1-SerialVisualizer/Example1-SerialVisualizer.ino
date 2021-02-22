d581dc3ea4e2f96f51af38bc6b42f77c3ec0ef40
Sean Miller <202.sean.202@gmail.com>
2021-02-21 20:02:22 -0500

    Thermopile device temperature test working

---
+++ Example1-SerialVisualizer/Example1-SerialVisualizer.ino
@@ -0,0 +1,120 @@
+/*
+  Using the Panasonic Grid-EYE Sensor
+  By: Nick Poole
+  SparkFun Electronics
+  Date: January 12th, 2018
+  
+  MIT License: Permission is hereby granted, free of charge, to any person obtaining a copy of this 
+  software and associated documentation files (the "Software"), to deal in the Software without 
+  restriction, including without limitation the rights to use, copy, modify, merge, publish, 
+  distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the 
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all copies or 
+  substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  
+  Feel like supporting our work? Buy a board from SparkFun!
+  https://www.sparkfun.com/products/14568
+  
+  This example implements a rudimentary heat camera in the serial terminal, using a matrix of ascii
+  characters to represent the temperature of each pixel in the device. Start your terminal at 115200 
+  and make the window as small as possible for best effect.
+  
+  Hardware Connections:
+  Attach the Qwiic Shield to your Arduino/Photon/ESP32 or other
+  Plug the sensor onto the shield
+*/
+
+#include <SparkFun_GridEYE_Arduino_Library.h>
+#include <Wire.h>
+
+// Use these values (in degrees C) to adjust the contrast
+#define HOT 40
+#define COLD 20
+
+// This table can be of type int because we map the pixel 
+// temperature to 0-3. Temperatures are reported by the 
+// library as floats
+int pixelTable[64];
+
+GridEYE grideye;
+
+void setup() {
+
+  // Start your preferred I2C object 
+  Wire.begin();
+  // Library assumes "Wire" for I2C but you can pass something else with begin() if you like
+  grideye.begin();
+  // Pour a bowl of serial
+  SerialUSB.begin(9600);
+  Serial1.begin(9600); // Initialize hardware serial port, pins 0/1 
+
+  while (!SerialUSB) ; // Wait for Serial monitor to open
+
+  // Send a welcome message to the serial monitor:
+  SerialUSB.println("Beginning Operations!");
+}
+
+float temp = 0.0;
+
+void loop() {
+
+  temp = grideye.getDeviceTemperature();
+  SerialUSB.print("The temperature of the device is:");
+  SerialUSB.println(temp);
+
+//  // loop through all 64 pixels on the device and map each float value to a number
+//  // between 0 and 3 using the HOT and COLD values we set at the top of the sketch
+//  for(unsigned char i = 0; i < 64; i++){
+//    SerialUSB.println(i);
+//    SerialUSB.println("The temperature at this pixel is:");
+//    pixelTable[i] = grideye.getPixelTemperature(i);
+//    SerialUSB.println(pixelTable[i]);
+//
+////    pixelTable[i] = map(grideye.getPixelTemperature(i), COLD, HOT, 0, 3);
+//  }
+
+//  for(unsigned char i = 0; i < 64; i++){
+//    SerialUSB.println(i);
+//    SerialUSB.println("The temperature at this pixel is:");
+//    SerialUSB.println(pixelTable[i]);
+//  }
+
+  
+//
+//
+//  // loop through the table of mapped values and print a character corresponding to each
+//  // pixel's temperature. Add a space between each. Start a new line every 8 in order to 
+//  // create an 8x8 grid
+//  for(unsigned char i = 0; i < 64; i++){
+//    if(pixelTable[i]==0){SerialUSB.println(".");}
+//    else if(pixelTable[i]==1){SerialUSB.println("o");}
+//    else if(pixelTable[i]==2){SerialUSB.println("0");}
+//    else if(pixelTable[i]==3){SerialUSB.println("O");}
+//    else { SerialUSB.println("G");}
+//    SerialUSB.println(" ");
+//    if((i+1)%8==0){
+//      SerialUSB.println();
+//    }
+//    SerialUSB.println("Test");
+//  }
+//
+//  // in between updates, throw a few linefeeds to visually separate the grids. If you're using
+//  // a serial terminal outside the Arduino IDE, you can replace these linefeeds with a clearscreen
+//  // command
+//  SerialUSB.println();
+//  SerialUSB.println();
+
+  SerialUSB.println("We in the loop I promise....");
+
+
+  // toss in a delay because we don't need to run all out
+  delay(200);
+
+}
--- algorithm/algorithm.ino
+++ algorithm/algorithm.ino
@@ -1,29 +1,28 @@
+#include "Arduino.h"
+
 /**
- *
- * MTE 482 - Final Design Project
- * iCross, An Automated Pedestrian Crossing System
- * By: Sean Miller, Thomas ten Kortenaar, Taylor Robertson
- * 2021-02-15
- * 
- */
+
+   MTE 482 - Final Design Project
+   iCross, An Automated Pedestrian Crossing System
+   By: Sean Miller, Thomas ten Kortenaar, Taylor Robertson
+   2021-02-15
+
+*/
 
 // TODO - put in the proper thermopile pin once the electronics have been configured
-const int THERMO_IN = 13; // Input analog pin for the thermopile input sensor
+//const int THERMO_IN = 13; // Input analog pin for the thermopile input sensor
 
-// TODO - put in the proper pressure pins once the electronics have been configured
-const int PRESSURE_IN_1 = 14; // Input analog pin for pressure input sensor 1/5
-const int PRESSURE_IN_2 = 15; // Input analog pin for pressure input sensor 2/5
-const int PRESSURE_IN_3 = 16; // Input analog pin for pressure input sensor 3/5
-const int PRESSURE_IN_4 = 17; // Input analog pin for pressure input sensor 4/5
-const int PRESSURE_IN_5 = 18; // Input analog pin for pressure input sensor 5/5
-
-// TODO - put the proper output pin once the electronics have been configured
-const int SIGNAL_OUT = 19 // Output digital pin for whether a pedestrian was detected or not
+const int PRESSURE_IN_1 = A0; // Input analog pin for pressure input sensor 1/4
+const int PRESSURE_IN_2 = A1; // Input analog pin for pressure input sensor 2/4
+const int PRESSURE_IN_3 = A2; // Input analog pin for pressure input sensor 3/4
+const int PRESSURE_IN_4 = A3; // Input analog pin for pressure input sensor 4/4
+const int LED_OUT = 8; // Output digital pin for whether a pedestrian was detected or not
+const int BUZZER_OUT = 9; // Output digital pin for whether a pedestrian was detected or not
 
 // TODO - decide on which weights/thresholds work best for this application
-const float PRESSURE_WEIGHT = 0.3;
+const float PRESSURE_WEIGHT = 0.75;
 const float THERMO_WEIGHT = 1.0 - PRESSURE_WEIGHT;
-const float SENSOR_THRESHOLD = 0.3;
+const float SENSOR_THRESHOLD = 0.25;
 
 
 // Boolean value representing the state of whether a pedestrian has been detected or not
@@ -31,7 +30,7 @@ bool pedestrianDetected = LOW;
 
 // State variables for all the input sensor values
 int thermo = 0;
-int pressure[5] = [];
+int pressure[4];
 
 // Intermediate values used in algorithm
 int pressure1st = 0;
@@ -40,31 +39,43 @@ float pressureAvg = 0.0;
 float sensorAvg = 0.0;
 
 // Set up the pins within setup()
-void setup() 
+void setup()
 {
+  SerialUSB.begin(9600); // Initialize Serial Monitor USB
+  Serial1.begin(9600); // Initialize hardware serial port, pins 0/1
+
+  while (!SerialUSB) ; // Wait for Serial monitor to open
+
+  // Send a welcome message to the serial monitor:
+  SerialUSB.println("Beginning Operations!");
+
+
   // Set all the input pins to be inputs
-  pinMode(THERMO_IN, INPUT);
+  //  pinMode(THERMO_IN, INPUT);
   pinMode(PRESSURE_IN_1, INPUT);
   pinMode(PRESSURE_IN_2, INPUT);
   pinMode(PRESSURE_IN_3, INPUT);
   pinMode(PRESSURE_IN_4, INPUT);
-  pinMode(PRESSURE_IN_5, INPUT);
-
-  // Set the output pin to be an output
-  pinMode(SIGNAL_OUT, OUTPUT);
+  
+    // Set the output pins to be an output
+  pinMode(LED_OUT, OUTPUT);
+  pinMode(BUZZER_OUT, OUTPUT);
 }
 
 
 // Continous loop that will run detecting pedestrians 24/7
-void loop() 
+void loop()
 {
   // Read in the sensor values from the input pins
-  thermo = analogRead(THERMO_IN);
-  pressure[0] = analogRead(PRESSURE_IN_1);
-  pressure[1] = analogRead(PRESSURE_IN_2);
-  pressure[2] = analogRead(PRESSURE_IN_3);
-  pressure[3] = analogRead(PRESSURE_IN_4);
-  pressure[4] = analogRead(PRESSURE_IN_5);
+  //  thermo = analogRead(THERMO_IN);
+    pressure[0] = analogRead(PRESSURE_IN_1) / 1024.0;
+    pressure[1] = analogRead(PRESSURE_IN_2)/ 1024.0;
+    pressure[2] = analogRead(PRESSURE_IN_3)/ 1024.0;
+    pressure[3] = analogRead(PRESSURE_IN_4)/ 1024.0;
+
+//  int val = analogRead(A0);
+//  SerialUSB.println("The value of the pressure sensor is:");
+//  SerialUSB.println(val);
 
   // Only use the 2 highest readings from the pressure sensor readings
   for (int i = 0; i < 5; i++) {
@@ -82,10 +93,10 @@ void loop()
   }
 
   // Take the average of the 2 highest pressure sensor values
-  pressureAvg = (pressure1 + pressure2) / 2.0;
+  pressureAvg = (pressure1st + pressure2nd) / 2.0;
 
   // The finalized sensor average wil be the weighted average of the 2 sensor types
-  sensorAvg = PRESSURE_wEIGHT*pressureAvg + THERMO_WEIGHT*thermo;
+  sensorAvg = PRESSURE_WEIGHT * pressureAvg + THERMO_WEIGHT * thermo;
 
   if (sensorAvg >= SENSOR_THRESHOLD) {
     pedestrianDetected = HIGH;
@@ -94,8 +105,13 @@ void loop()
   }
 
   // write the value to the output pin
-  digitalWrite(SIGNAL_OUT, pedestrianDetected);
-
+  //  digitalWrite(SIGNAL_OUT, pedestrianDetected);
+  SerialUSB.println("Pedestrian Detected?");
+  SerialUSB.println(pedestrianDetected);
+  digitalWrite(LED_OUT, HIGH);
+  tone(BUZZER_OUT, 250);
+  digitalWrite(BUZZER_OUT, HIGH);
+  
   // run a small delay to preserve processor power
-  delay(200);
+//  delay(200);
 }
